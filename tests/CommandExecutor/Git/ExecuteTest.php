479a870c0b7e3f803fdc2a7070c427f700a1e5be
ptlis <ptlis@ptlis.net>
2014-12-20 13:00:18 +0000

    Add: Tests for git CommandExecutor.

---
+++ tests/CommandExecutor/Git/ExecuteTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * PHP Version 5.4
+ *
+ * @copyright (c) 2014 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\Vcs\Test\CommandExecutor\Git;
+
+use ptlis\Vcs\Git\CommandExecutor;
+
+class ExecuteTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        // For this test we just use the first commit of this project
+        $executor = new CommandExecutor('/usr/bin/git', '.');
+
+        $output = $executor->execute(array(
+            'log',
+            '--reverse',
+            '--format=fuller'
+        ));
+
+
+        $this->assertEquals(
+            'commit 6f1ed5364b1369b618270b2774f6ec86f77fa213',
+            $output[0]
+        );
+    }
+}
