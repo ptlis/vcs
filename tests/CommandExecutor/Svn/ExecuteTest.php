4ba18c5c3e4ac306ca9ea40501e9e8d9032e8209
ptlis <ptlis@ptlis.net>
2014-12-20 18:49:44 +0000

    Add: Tests for svn CommandExecutor.

---
+++ tests/CommandExecutor/Svn/ExecuteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * PHP Version 5.4
+ *
+ * @copyright (c) 2014 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\Vcs\Test\CommandExecutor\Svn;
+
+use ptlis\Vcs\Svn\CommandExecutor;
+
+class ExecuteTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExecute()
+    {
+        // For this test we just use the first commit of this project
+        $executor = new CommandExecutor('/usr/bin/svn', '.');
+
+        $output = $executor->execute(array(
+            'help'
+        ));
+
+
+        $this->assertEquals(
+            'usage: svn <subcommand> [options] [args]',
+            $output[0]
+        );
+    }
+}
