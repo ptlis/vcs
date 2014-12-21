d72df3d7300906af6e22135906b6dc003e471243
ptlis <ptlis@ptlis.net>
2014-12-21 18:11:14 +0000

    Fix: Add basic test for RepositoryConfig object.

---
+++ tests/RepositoryConfigTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * PHP Version 5.4
+ *
+ * @copyright (c) 2014 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\Vcs\Test;
+
+
+use ptlis\Vcs\Svn\RepositoryConfig;
+
+class RepositoryConfigTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetters()
+    {
+        $repoConfig = new RepositoryConfig();
+
+        $this->assertEquals('trunk', $repoConfig->getTrunkName());
+        $this->assertEquals('tags', $repoConfig->getTagRootDir());
+        $this->assertEquals('branches', $repoConfig->getBranchRootDir());
+    }
+}
