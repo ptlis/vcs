3d3d571f7ed0b5ea3dd461fca59301229f3ec719
ptlis <ptlis@ptlis.net>
2014-12-23 21:10:08 +0000

    Add: Tests for SvnVcs class.

---
+++ tests/Vcs/Svn/GetMetaTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * PHP Version 5.4
+ *
+ * @copyright (c) 2014 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\Vcs\Test\Vcs\Git;
+
+use ptlis\Vcs\Git\GitVcs;
+use ptlis\Vcs\Test\MockCommandExecutor;
+
+class GetMetaTest extends \PHPUnit_Framework_TestCase
+{
+    public function testBranchExists()
+    {
+        $commandExecutor = new MockCommandExecutor(array());
+
+        $vcs = new GitVcs($commandExecutor);
+
+        $meta = $vcs->getMeta();
+
+        $this->assertInstanceOf('\ptlis\Vcs\Interfaces\MetaInterface', $meta);
+    }
+}
